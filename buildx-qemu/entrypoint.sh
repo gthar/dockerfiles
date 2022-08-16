ff963159a95a6203d53bb43f9851543d913458f1
Ricard Illa <rilla@monotremata.xyz>
2022-08-17 00:15:44 +0200

    make buildx instance use host network to keep dns as expected

--- buildx-qemu/entrypoint.sh
+++ buildx-qemu/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 update-binfmts --enable
-docker buildx create --use
+docker buildx create --driver docker-container --driver-opt network=host --name image-builder --use
 docker buildx inspect --bootstrap
 
 exec "$@"
